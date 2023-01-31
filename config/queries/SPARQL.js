beba549f9b896acfbac4d45c8e0020af6a2dacf4
Julian Rojas <julian.rojas87@gmail.com>
2023-01-31 21:49:42 +0100

    Add 4th query for handling incoming and outgoing border links

--- config/queries/SPARQL.js
+++ config/queries/SPARQL.js
@@ -141,7 +141,7 @@ export default {
                 `;
             },
             (lat1, long1, lat2, long2) => {
-                // Query for SoL related topology nodes at the borders of the bbox
+                // Query for outgoing SoL related topology nodes at the borders of the bbox
                 return `
                 PREFIX era: <http://data.europa.eu/949/>
                 PREFIX era-nv: <http://data.europa.eu/949/concepts/navigabilities/rinf/>
@@ -202,6 +202,80 @@ export default {
                         ?nr2 a era:NetRelation;
                             era:elementA ?opne;
                             era:elementB ?solne;
+                            era:navigability ?navBA.
+                    }
+                    
+                    # Conditions for OP to be inside the tile in question
+                    FILTER(?inLong >= ${long1} && ?inLong <= ${long2})
+                    FILTER(?inLat <= ${lat1} && ?inLat >= ${lat2})
+    
+                    # Condition for OP to be outside the tile in question
+                    FILTER(?outLong < ${long1} || ?outLong > ${long2} || ?outLat > ${lat1} || ?outLat < ${lat2})
+                }            
+                `;
+            },
+            (lat1, long1, lat2, long2) => {
+                // Query for incoming SoL related topology nodes at the borders of the bbox
+                return `
+                PREFIX era: <http://data.europa.eu/949/>
+                PREFIX era-nv: <http://data.europa.eu/949/concepts/navigabilities/rinf/>
+                PREFIX wgs: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+                PREFIX gsp: <http://www.opengis.net/ont/geosparql#>
+                CONSTRUCT {
+                    ?opne gsp:asWKT ?opWkt;
+                        era:length ?opLength;
+                        era:linkedTo ?solne.
+                    ?solne era:length ?solLength;
+                        gsp:asWKT ?solWkt.
+                } 
+                FROM <http://data.europa.eu/949/graph/rinf>
+                WHERE {        
+                    ?inOP a era:OperationalPoint;
+                        wgs:location [ 
+                            wgs:lat ?inLat; 
+                            wgs:long ?inLong 
+                    ].
+                    ?outOP a era:OperationalPoint;
+                        era:hasAbstraction ?mesoOPNe;
+                        wgs:location [
+                            wgs:lat ?outLat;
+                            wgs:long ?outLong;
+                            gsp:asWKT ?opWkt 
+                    ].
+                    ?SoL a era:SectionOfLine;
+                        era:hasAbstraction ?solMesoNe.
+                    {
+                        ?SoL era:opStart ?inOP;
+                            era:opEnd ?outOP.
+                    }
+                    UNION
+                    {
+                        ?SoL era:opStart ?outOP;
+                            era:opEnd ?inOP.
+                    }
+                    
+                    ?opne ^era:elementPart ?mesoOPNe.
+                    OPTIONAL { ?opne era:length ?nelength }
+                    BIND(COALESCE(?nelength, 1.0) AS ?opLength)
+                    
+                    ?solne ^era:elementPart ?solMesoNe;
+                            era:length ?solLength.
+                    BIND(CONCAT("POINT(", STR((?inLong + ?outLong) / 2), " ", STR((?inLat + ?outLat) / 2), ")") AS ?solWkt)
+
+                    VALUES ?navAB { era-nv:AB era-nv:Both }
+                    VALUES ?navBA { era-nv:BA era-nv:Both }
+
+                    {
+                        ?nr1 a era:NetRelation;
+                            era:elementA ?opne;
+                            era:elementB ?solne;
+                            era:navigability ?navAB.
+                    }
+                    UNION
+                    {
+                        ?nr2 a era:NetRelation;
+                            era:elementA ?solne;
+                            era:elementB ?opne;
                             era:navigability ?navBA.
                     }
                     
@@ -352,7 +426,7 @@ export default {
                 `;
             },
             (lat1, long1, lat2, long2) => {
-                // Query for SoL related topology nodes at the borders of the bbox
+                // Query for outgoing SoL related topology nodes at the borders of the bbox
                 // Here we apply both workarounds of the previous queries. 
                 return `
                 PREFIX era: <http://data.europa.eu/949/>
@@ -414,6 +488,81 @@ export default {
                         ?nr2 a era:NetRelation;
                             era:elementA ?opne;
                             era:elementB ?solne;
+                            era:navigability ?navBA.
+                    }
+                    
+                    # Conditions for OP to be inside the tile in question
+                    FILTER(?inLong >= ${long1} && ?inLong <= ${long2})
+                    FILTER(?inLat <= ${lat1} && ?inLat >= ${lat2})
+    
+                    # Condition for OP to be outside the tile in question
+                    FILTER(?outLong < ${long1} || ?outLong > ${long2} || ?outLat > ${lat1} || ?outLat < ${lat2})
+                }            
+                `;
+            },
+            (lat1, long1, lat2, long2) => {
+                // Query for incoming SoL related topology nodes at the borders of the bbox
+                // Here we apply both workarounds of the previous queries. 
+                return `
+                PREFIX era: <http://data.europa.eu/949/>
+                PREFIX era-nv: <http://data.europa.eu/949/concepts/navigabilities/rinf/>
+                PREFIX wgs: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+                PREFIX gsp: <http://www.opengis.net/ont/geosparql#>
+                CONSTRUCT {
+                    ?opne gsp:asWKT ?opWkt;
+                        era:length 1.0;
+                        era:linkedTo ?solne.
+                    ?solne era:length ?solLength;
+                        wgs:lat ?solLat;
+                        wgs:long ?solLong.
+                } 
+                WHERE {        
+                    ?inOP a era:OperationalPoint;
+                        wgs:location [ 
+                            wgs:lat ?inLat; 
+                            wgs:long ?inLong 
+                        ].
+                    ?outOP a era:OperationalPoint;
+                        era:hasAbstraction ?mesoOPNe;
+                            wgs:location [
+                            wgs:lat ?outLat;
+                            wgs:long ?outLong;
+                            gsp:asWKT ?opWkt 
+                        ].
+                    ?SoL a era:SectionOfLine;
+                        era:hasAbstraction ?solMesoNe.
+
+                    {
+                        ?SoL era:opStart ?inOP;
+                            era:opEnd ?outOP.
+                    }
+                    UNION
+                    {
+                        ?SoL era:opStart ?outOP;
+                            era:opEnd ?inOP.
+                    }
+
+                    ?opne ^era:elementPart ?mesoOPNe.
+
+                    ?solne ^era:elementPart ?solMesoNe;
+                            era:length ?solLength.
+                    
+                    { BIND((?inLong + ?outLong)/2 AS ?solLong) } UNION { BIND((?inLat + ?outLat)/2 AS ?solLat) }
+                    
+                    VALUES ?navAB { era-nv:AB era-nv:Both }
+                    VALUES ?navBA { era-nv:BA era-nv:Both }
+
+                    {
+                        ?nr1 a era:NetRelation;
+                            era:elementA ?opne;
+                            era:elementB ?solne;
+                            era:navigability ?navAB.
+                    }
+                    UNION
+                    {
+                        ?nr2 a era:NetRelation;
+                            era:elementA ?solne;
+                            era:elementB ?opne;
                             era:navigability ?navBA.
                     }
                     
