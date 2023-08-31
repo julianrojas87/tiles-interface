90cc8952418ed0723dc8dcaa81a9e6e978d77e56
Julian Rojas <julian.rojas87@gmail.com>
2023-08-31 17:05:02 +0200

    Adjust OSM queries to fix incomplete results issue

--- config/queries/examples/OSM-SPARQL.js
+++ config/queries/examples/OSM-SPARQL.js
@@ -24,8 +24,10 @@ export default {
             `
         },
         queries: [
+            // We split unidirectional and bidirectional into 2 queries for performance reasons
             (lat1, long1, lat2, long2) => {
-                // GeoSPARQL query for all osm:Nodes and their connections within the given bbox
+                // GeoSPARQL query for all osm:Nodes and 
+                // their unidirectional connections starting from the given bbox
                 return `
                 PREFIX osm: <https://w3id.org/openstreetmap/terms#>
                 PREFIX gsp: <http://www.opengis.net/ont/geosparql#>
@@ -34,82 +36,55 @@ export default {
                 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
                 PREFIX rt: <http://w3id.org/routable-tiles/terms#>
                 CONSTRUCT {
-                    ?nodeA gsp:asWKT ?wktA.
-                    ?nodeB gsp:asWKT ?wktB.
-                    ?nodeC gsp:asWKT ?wktC.
-                    ?nodeD gsp:asWKT ?wktD.
+                    ?node gsp:asWKT ?wkt;
+                        rt:linkedTo ?n2.
 
-                    ?nodeA rt:biLinkedTo ?nodeB.
-                    ?nodeC rt:linkedTo ?nodeD.
+                    ?n2 gsp:asWKT ?wkt2.
                 }
                 FROM <http://belgium.roads.osm>
                 WHERE {
-                    # Get location of the first element of the list
-                    ?way a osm:Way;
-                        osm:hasNodes ?initList.
+                    # Node that belongs to the tile in question
+                    ?node a osm:Node;
+                        wgs:lat ?lat;
+                        wgs:long ?long;
+                        gsp:asWKT ?wkt.
+
+                # Select only Nodes that are part of certain types of Ways
+                    VALUES ?roadTypes { 
+                        osm:Motorway 
+                        osm:Trunk 
+                        osm:Primary 
+                        osm:Secondary
+                        osm:Tertiary
+                        osm:Residential 
+                        osm:Unclassified
+                        osm:Crossing
+                    }
                     
-                    ?initList rdf:first [ wgs:lat ?lat; wgs:long ?long ].
+                    ?way a osm:Way;
+                        osm:highway ?roadTypes;
+                        osm:hasNodes ?nodeList.
 
-                    {
-                        # Get consecutive nodes with their location
-                        ?nodeA ^rdf:first/rdf:rest/rdf:first ?nodeB;
-                            gsp:asWKT ?wktA.
+                    # Make sure the tile node is part of the Ways we want
+                    ?nodeList rdf:rest*/rdf:first ?node.
+                    
+                    # Get consecutive nodes that are unidirectional
+                    ?way osm:hasTag "oneway=yes".
 
-                        ?nodeB gsp:asWKT ?wktB.
+                    ?n2 a osm:Node;
+                        gsp:asWKT ?wkt2.
 
-                        # Make sure they are related to the local osm:Way
-                        ?initList rdf:rest*/rdf:first ?nodeA.
-
-                        # Query only for bidirectional osm:Ways
-                        FILTER NOT EXISTS {
-                            ?way osm:hasTag "oneway=yes".
-                        }
-                    }
-                    UNION
-                    {
-                        # Get consecutive nodes with their location
-                        ?nodeC ^rdf:first/rdf:rest/rdf:first ?nodeD;
-                            gsp:asWKT ?wktC.
-
-                        ?nodeD gsp:asWKT ?wktD.
-
-                        # Make sure they are related to the local osm:Way
-                        ?initList rdf:rest*/rdf:first ?nodeC.
-
-                        # Query only for unidirectional osm:Ways
-                        FILTER EXISTS {
-                            ?way osm:hasTag "oneway=yes".
-                        }
-                    }
+                    ?node ^rdf:first/rdf:rest/rdf:first ?n2.
                     
                     # Tile geospatial filter
                     FILTER(?long >= ${long1} && ?long <= ${long2})
                     FILTER(?lat <= ${lat1} && ?lat >= ${lat2})
                 }
                 `;
-            }
-        ]
-    },
-    graphdb: {
-        location: (id) => {
-            return `
-            PREFIX osm: <https://w3id.org/openstreetmap/terms#>
-            PREFIX gsp: <http://www.opengis.net/ont/geosparql#>
-            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-            SELECT ?id ?label ?wkt
-            WHERE {
-                BIND(<${id}> AS ?id)
-                ?id a osm:Node;
-                    gsp:asWKT ?wkt.
-
-                OPTIONAL {
-                    ?id ^rdf:first/^rdf:rest*/^osm:hasNodes [ osm:name ?label ].
-                }
-            }
-            `
-        },
-        queries: [
+            },
             (lat1, long1, lat2, long2) => {
+                // GeoSPARQL query for all osm:Nodes and 
+                // their bidirectional connections starting from the given bbox
                 return `
                 PREFIX osm: <https://w3id.org/openstreetmap/terms#>
                 PREFIX gsp: <http://www.opengis.net/ont/geosparql#>
@@ -118,59 +93,52 @@ export default {
                 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
                 PREFIX rt: <http://w3id.org/routable-tiles/terms#>
                 CONSTRUCT {
-                    ?nodeA gsp:asWKT ?wktA.
-                    ?nodeB gsp:asWKT ?wktB.
-                    ?nodeC gsp:asWKT ?wktC.
-                    ?nodeD gsp:asWKT ?wktD.
+                    ?node gsp:asWKT ?wkt;
+                        rt:biLinkedTo ?n2.
 
-                    ?nodeA rt:biLinkedTo ?nodeB.
-                    ?nodeC rt:linkedTo ?nodeD.
+                    ?n2 gsp:asWKT ?wkt2.
                 }
                 FROM <http://belgium.roads.osm>
                 WHERE {
-                    # Get location of the first element of the list
-                    ?way a osm:Way;
-                        osm:hasNodes ?initList.
+                    # Node that belongs to the tile in question
+                    ?node a osm:Node;
+                        wgs:lat ?lat;
+                        wgs:long ?long;
+                        gsp:asWKT ?wkt.
+
+                # Select only Nodes that are part of certain types of Ways
+                    VALUES ?roadTypes { 
+                        osm:Motorway 
+                        osm:Trunk 
+                        osm:Primary 
+                        osm:Secondary
+                        osm:Tertiary
+                        osm:Residential 
+                        osm:Unclassified
+                        osm:Crossing
+                    }
                     
-                    ?initList rdf:first [ wgs:lat ?lat; wgs:long ?long ].
+                    ?way a osm:Way;
+                        osm:highway ?roadTypes;
+                        osm:hasNodes ?nodeList.
 
-                    {
-                        # Get consecutive nodes with their location
-                        ?nodeA ^rdf:first/rdf:rest/rdf:first ?nodeB;
-                            gsp:asWKT ?wktA.
+                    # Make sure the tile node is part of the Ways we want
+                    ?nodeList rdf:rest*/rdf:first ?node.
+                    
+                    
+                    # Get consecutive nodes that are bidirectional
+                    FILTER NOT EXISTS { ?way osm:hasTag "oneway=yes" }
 
-                        ?nodeB gsp:asWKT ?wktB.
+                    ?n2 a osm:Node;
+                        gsp:asWKT ?wkt2.
 
-                        # Make sure they are related to the local osm:Way
-                        ?initList rdf:rest*/rdf:first ?nodeA.
-
-                        # Query only for bidirectional osm:Ways
-                        FILTER NOT EXISTS {
-                            ?way osm:hasTag "oneway=yes".
-                        }
-                    }
-                    UNION
-                    {
-                        # Get consecutive nodes with their location
-                        ?nodeC ^rdf:first/rdf:rest/rdf:first ?nodeD;
-                            gsp:asWKT ?wktC.
-
-                        ?nodeD gsp:asWKT ?wktD.
-
-                        # Make sure they are related to the local osm:Way
-                        ?initList rdf:rest*/rdf:first ?nodeC.
-
-                        # Query only for unidirectional osm:Ways
-                        FILTER EXISTS {
-                            ?way osm:hasTag "oneway=yes".
-                        }
-                    }
+                    ?node ^rdf:first/rdf:rest/rdf:first ?n2.
                     
                     # Tile geospatial filter
                     FILTER(?long >= ${long1} && ?long <= ${long2})
                     FILTER(?lat <= ${lat1} && ?lat >= ${lat2})
                 }
-                `;
+                `
             }
         ]
     }
